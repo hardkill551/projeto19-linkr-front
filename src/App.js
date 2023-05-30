e7ca4793eee526363123d11f7cbeb8404551c66e
Bianca Dias <bianca.dias@ufv.br>
2023-05-30 19:56:06 -0300

    feat userPage: layout estático da pagina do usuario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import TimelinePage from "./pages/TimelinePage";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/timeline" element={<TimelinePage />} />
+        <Route path="/user/:id" element={< UserPage />} />
       </Routes>
     </BrowserRouter>
   );
